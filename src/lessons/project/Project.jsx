dbff4c985a50b8447098a5fc13cd20ed1c23e8e8
kiethohoai <kietho.060193@gmail.com>
2024-06-13 14:48:02 +0700

    39 Normalize Library

--- src/lessons/project/Project.jsx
+++ src/lessons/project/Project.jsx
@@ -1,13 +1,13 @@
 import Login from "./Login"
 import './Project.scss';
 import "nprogress/nprogress.css";
-import Lesson36 from "./chapter4/lesson36/Lesson36";
+import Lesson39 from "./chapter4/lesson39/Lesson39";
 
 const Project = (props) => {
   return (
     <div className="project">
       <Login /> <br />
-      <Lesson36 />
+      <Lesson39 />
     </div>
   );
 };
